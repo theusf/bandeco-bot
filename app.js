e104af4e535ca92aab471ede73068a501ae7e540
Matheus Francisco <matheusfrancisco@MacBook-Pro-de-Rodrigo.local>
2020-01-14 12:54:00 -0300

    changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var app = require("./config/server.js");
 var cliente = require("./config/twitter.js"); //Importa o arquivo twitter.js
+var CronJob = require('cron').CronJob; //Importa o "node-cron"
 
 
 //Configura a porta disponível ou a porta 3000
@@ -11,8 +12,24 @@ app.listen(server_port, server_host, function () {
     console.log("Aplicação online.");
 });
 
+var job = new CronJob('00 10 08 * * 1-7', function () {
+    //Roda todo dia às 0:10:00
+
+    cliente.tweetar("bom dia vamo acorda     https://twitter.com/i/status/1217102231683653633");
+
+},
+    function () {
+        //Função executada quando o job finaliza
+        console.log("Cron job stopped!")
+    },
+    true, //Ativa o job
+    'America/Sao_Paulo' //Fuso horário.
+);
+
+
 app.get("/posta", function (req, res) {
     console.log('Fez um get!')
 
     cliente.tweetar("bom dia vamo acorda     https://twitter.com/i/status/1217102231683653633");
+
 });
