25a2f0c4a25b5b1aab7803243ba9e93200e9375c
Matheus Francisco <matheusfrancisco@MacBook-Pro-de-Francisco.local>
2020-03-16 13:40:56 -0300

    added consult with date on url

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ var app = require("./config/server.js");
 var cliente = require("./config/twitter.js"); //Importa o arquivo twitter.js
 var CronJob = require('cron').CronJob; //Importa o "node-cron"
 var http = require('http');
+
 const request = require('request');
 const cheerio = require('cheerio');
-const url = 'http://www.prefeitura.unicamp.br/apps/site/cardapio.php';
 
-//Configura a porta disponível ou a porta 3000
-var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
-//Configura o host disponível ou "0.0.0.0"
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+const base_url = 'http://www.prefeitura.unicamp.br/apps/site/cardapio.php';
 
-app.listen(server_port, server_host, function () {
-    console.log("Aplicação online.");
-});
 
-const getPage = ( cb ) => {
+const titulo_refeicoes = {
+    0: 'Almoço 🍽️',
+    1: 'Almoço Vegetariano 🥗🍴',
+    2: 'Jantar 🍲',
+    3: 'Jantar Vegetariano 🥗🍴'
+}
+
+
+const getPage = ( url, cb ) => {
     request(url,  {
         timeout: 3000,
         encoding: "latin1"
@@ -44,9 +46,13 @@ const  parsePage = ( data ) => {
         $titulo.text();
 
        const obj = {
-        TITULO: $titulo.text(),
+        TITULO: '',
+        DATA: '',
         REFEICAO: ''
        }
+
+       obj['DATA'] = $titulo.text().split('s - ').pop();
+       obj['TITULO'] = titulo_refeicoes[i]
 
        $td.each(async (i, elem) => {
 
@@ -66,7 +72,6 @@ const  parsePage = ( data ) => {
             }       
         })
 
-
         output.push(obj)
 
     });
@@ -75,38 +80,33 @@ const  parsePage = ( data ) => {
 
 const post = () => {
 
-    getPage( (html) => {
+    getPage( base_url, (html) => {
         let data = parsePage(html);
-    
-        const emojis = {
-            ABACAXI: '🍍',
-            MAÇA: '🍎',
-            MANGA: '🥭',
-            UVA: '🍇',
-            TANGERINA: '🍊',
-            LARANJA: '🍊',
-            LIMÃO: '🍋'
-        }
-    
-        const titulo = `${data[0]['TITULO']}`
-    
-        const titulo_refeicoes = {
-            0: 'Almoço 🍽️',
-            1: 'Almoço Vegetariano 🥗🍴',
-            2: 'Jantar 🍲',
-            3: 'Jantar Vegetariano 🥗🍴'
-        }
-    
-        for (var i = 0; i<4 ; i++ ) {
-            const suco =  data[i]['SUCO:'];
-            const tweet = `${titulo}\r\n${titulo_refeicoes[i]}\r\n${data[i]['REFEICAO']}\r\n \r\n PRATO PRINCIPAL: ${data[i]['PRATO PRINCIPAL:']}\r\n \r\nSALADA: ${data[i]['SALADA:']}\r\n \r\nSOBREMESA: ${data[i]['SOBREMESA:']}\r\n \r\nSUCO: ${suco} ${(emojis[suco] ? emojis[suco] : '') }`
-            console.log(tweet)
-            cliente.tweetar(tweet);
+
+        if (data.length === 0)
+            cliente.tweetar('Sem bandeco hoje 😔🤚');
+        else {
+            const emojis = {
+                ABACAXI: '🍍',
+                MAÇA: '🍎',
+                MANGA: '🥭',
+                UVA: '🍇',
+                TANGERINA: '🍊',
+                LARANJA: '🍊',
+                LIMÃO: '🍋'
+            }
+        
+            for (var i = 0; i<4 ; i++ ) {
+                const suco =  data[i]['SUCO:'];
+                const tweet = `${data[i]['DATA']}\r\n${titulo_refeicoes[i]}\r\n${data[i]['REFEICAO']}\r\n \r\nPRATO PRINCIPAL: ${data[i]['PRATO PRINCIPAL:']}\r\n \r\nSALADA: ${data[i]['SALADA:']}\r\n \r\nSOBREMESA: ${data[i]['SOBREMESA:']}\r\n \r\nSUCO: ${suco} ${(emojis[suco] ? emojis[suco] : '') }`
+                //console.log(tweet)
+                cliente.tweetar(tweet);
+            }
         }
     }
 )};
 
-
+//post();
 
 var job = new CronJob('00 30 8 * * 1-5', function () {
 //    Roda todo dia às 0:08:030
@@ -123,16 +123,16 @@ var job = new CronJob('00 30 8 * * 1-5', function () {
 );
 
 
-app.get("/posta", function (req, res) {
-    console.log('faz o post!')
+// app.get("/posta", function (req, res) {
+//     console.log('faz o post!')
 
-    post();
+//     post();
 
-});
+// });
 
 
-var keepAwake = new CronJob('0 */25 * * * *', function () {
-    //Roda de 25 em 25 minutos para manter o bot acordado
+var keepAwake = new CronJob('0 */23 * * * *', function () {
+    //Roda de 23 em 23 minutos para manter o bot acordado
 
     http.get('http://bandecobot.herokuapp.com')
 
@@ -148,13 +148,24 @@ var keepAwake = new CronJob('0 */25 * * * *', function () {
 
 
 app.get("/", function (req, res) {
-    console.log('Fez um get!')
 
-    getPage( (html) => {
+    let url = base_url;
+
+    if (req.query.data)
+        url = url + '?d=' + req.query.data;
+    
+    getPage( url, (html) => {
         const data = parsePage(html);
-        //console.log(data)
 
         res.send(JSON.stringify(data));
 
     });
 });
+
+
+app.get("/posta", function (req, res) {
+
+    post();
+
+});
+
--- config/server.js
+++ config/server.js
@@ -1,5 +1,14 @@
 var express = require("express");
 var app = express();
 
+//Configura a porta disponível ou a porta 3000
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+//Configura o host disponível ou "0.0.0.0"
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+app.listen(server_port, server_host, function () {
+    console.log("Aplicação online.");
+});
+
 //Exporta app
 module.exports = app;
